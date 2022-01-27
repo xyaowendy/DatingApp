5b359b81778d486e5661cd8c036394d526eceeff
Wendy Jiang <wendyjiang0611@gmail.com>
2022-01-28 00:51:32 +1000

    added validation to avoid username or password null situation

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs;
 
 public class RegisterDto
 {
+    // data annotations
+    [Required]
     public string Username { get; set; }
 
+    [Required]
     public string Password { get; set; }
 }
