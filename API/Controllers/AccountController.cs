736bd30f1bd31b6e86ddefa7b1aa99cbd750e4db
Wendy Jiang <wendyjiang0611@gmail.com>
2022-01-28 11:11:35 +1000

    added a login endpoint; cleared junk data in database

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -38,6 +38,26 @@ public class AccountController : BaseApiController
         return user; 
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+        if (user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized(("Invalid password"));
+        }
+
+        return user;
+    }
+
+
     // make username unique
     private async Task<bool> UserExits(string username)
     {
